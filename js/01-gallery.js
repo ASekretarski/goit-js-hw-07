a5659885f13aaf4377cec2b1a8378a76367688f9
ASekretarski <artur.sekretarski@gmail.com>
2023-05-19 17:09:01 +0200

    gallery rendered

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const gallery = document.querySelector('.gallery')
 console.log(galleryItems);
+console.log("__________________________")
+const galleryListItems = [];
+
+galleryItems.forEach(item => {
+
+    galleryListItems.push(`<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="Image description"
+    />
+  </a>
+</div>`);
+    console.log(item.description)
+    console.log(item.preview)
+    console.log(item.original)
+    console.log("__________________________")
+});
+
+console.log(galleryListItems.join(""))
+
+gallery.insertAdjacentHTML("beforeend", galleryListItems.join(""))
+
