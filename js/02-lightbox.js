a95e25cc617749a2dabd1bc10729140b25ac89d3
ASekretarski <artur.sekretarski@gmail.com>
2023-05-20 15:53:23 +0200

    task 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,8 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const gallery = document.querySelector('.gallery')
 const galleryListItems = [];
 
@@ -14,3 +12,15 @@ galleryItems.forEach(item => {
 
 gallery.insertAdjacentHTML("beforeend", galleryListItems.join(""))
 
+
+
+const allGalleryLinks = document.querySelectorAll('.gallery__item')
+
+for (const link of allGalleryLinks) {
+    link.addEventListener("click", function (event) {
+        event.preventDefault()
+        console.log("Hello world") 
+    }) 
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { captions:true, captionsData:'alt', captionDelay:250,  });
